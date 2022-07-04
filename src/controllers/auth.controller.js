81d23d35dfa7874d182ca2486522e57eee48cdd8
Sergio <webdev.slara@gmail.com>
2022-07-04 23:14:30 +0200

    added assignation of roles during the creation of a user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,29 @@
 import User from '../models/User.js'
+import Role from '../models/Role.js'
 import jwt from 'jsonwebtoken'
 import config from '../config.js'
 
 export const signUp = async (req, res) => {
-    const { username, email, password } = req.body
+    const { username, email, password, roles} = req.body
 
+    //creating new user
     const newUser = new User({
         username: username,
         email: email,
         password: await User.encryptPassword(password),
     })
 
-    const userSaved = await newUser.save()
+    //assigning roles to the users
+    if(roles){
+        const foundRoles = await Role.find({name: {$in: roles}})
+        newUser.roles = foundRoles.map(role => role._id)
+    }else{
+        const role = await Role.findOne({name: "user"})
+        newUser.roles = [role._id]
+    }
 
+    const userSaved = await newUser.save()
+    console.log(userSaved)
     //signing the token with jwt
     const token = jwt.sign({id: userSaved.id},config.SECRET,{
         expiresIn: 86400 //24 hours
