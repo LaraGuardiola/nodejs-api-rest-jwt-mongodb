d39fb8fab3e7b857a2568d892aee42f85e7795a8
Sergio <webdev.slara@gmail.com>
2022-07-05 17:01:01 +0200

    signin now validates if user is found on the db, and also checks ifs sent password matches the db any result of the db

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,5 +34,18 @@ export const signUp = async (req, res) => {
 }
 
 export const signIn = async (req, res) => {
-    res.json('signin')
+    
+    const userFound = await User.findOne({email: req.body.email}).populate("roles")
+    
+    //if user doesnt exist
+    if(!userFound) return res.status(400).json({message: "User not found"})
+
+    const matchPassword = await User.comparePassword(req.body.password, userFound.password)
+
+    //if password doesn't match
+    if(!matchPassword) return res.status(401).json({token: null, message: 'Invalid password'})
+
+    console.log(userFound)
+
+    res.json({token:''})
 }
